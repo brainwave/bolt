0b54153e27b0039dacda8e68e23792539633ff7a
Brahm Prakash Mishra <brahm@live.in>
2016-04-18 00:03:19 +0530

    Implemented readStl functionality as part of triangleMesh class

--- bolt_code/stlparse_opengl/main.cpp
+++ bolt_code/stlparse_opengl/main.cpp
@@ -1,4 +1,15 @@
 #include "meshDef.hpp"
 
-int main() {
+int main ( int argc, char *argv[] ) {
+
+	cout<<"\nProgram Starts, file name is : ";
+	cout<<argv[1];
+
+	triangleMesh mesh;
+
+	if ( mesh.readStlFile(argv[1]) )
+		cout << "\nProgram Failed" ;
+	
+	else
+		mesh.display_all_elements();
 }
--- bolt_code/stlparse_opengl/meshDef.hpp
+++ bolt_code/stlparse_opengl/meshDef.hpp
@@ -1,61 +1,40 @@
 #include <iostream>
 #include "libs/glm/glm.hpp"
+#include "libs/glm/gtx/string_cast.hpp"
 #include <vector>
 
-struct vec3 {
-
-	glm::vec3 pv; //position vector
-
-	vec3(float x, float y, float z) {
-		
-		pv.x=x;
-		pv.y=y;
-		pv.z=z;
-
-	}
-
-	friend vec3& operator>>(const vec3& read_vec, vec3& vec) {
-		
-		vec.pv.x = read_vec.pv.x;
-		vec.pv.y = read_vec.pv.y;
-		vec.pv.z = read_vec.pv.z;
-
-		return vec;
-	}
-
-	friend std::ostream& operator<<(std::ostream& output, const vec3 vec){
-		output<<round((vec.pv.x)*1000.0)/1000.0<<"i+"<<round((vec.pv.y)*1000.0)/1000.0<<"j+"<<round((vec.pv.z)*1000.0)/1000.0<<"k ";
-		return output;
-	}
-	
-	vec3 operator-(const vec3& vector);
-
-	vec3 operator+(const vec3& vector);
-
-	vec3 operator*(const float f);
-
-	float dot(const vec3& vec);
-	
-	void normalize();
-
-};
+using namespace std;
+using namespace glm;
 
 struct triangle {
 	
 	vec3 vertex[3], normal;
+
+	friend triangle& operator>>(const vec3 vertex[4], triangle &t){ 
+
+		t.normal = vertex[0];	
+		for(int i=0;i<3;i++) 
+			t.vertex[i] 	= vertex[i+1];
+		
+		//vertex[0] is normal, vertex 1, 2, 3 are the vertices. Read accordingly.
+		return t;
+	}
+	friend ostream& operator<<(ostream &output, const triangle &t) {
+		
+		output	<<"\nVertex 1 : "<<t.vertex[0].x<<"i+"<<t.vertex[0].y<<"j+"<<t.vertex[0].z<<"k"
+			<<"\nVertex 2 : "<<t.vertex[1].x<<"i+"<<t.vertex[1].y<<"j+"<<t.vertex[1].z<<"k"
+			<<"\nVertex 3 : "<<t.vertex[2].x<<"i+"<<t.vertex[2].y<<"j+"<<t.vertex[2].z<<"k"
+			<<"\nNormal   : "<<t.normal.x<<"i+"<<t.normal.y<<"j+"<<t.normal.z<<"k";
+		
+		return output;
+	}
 	
-	friend triangle& operator>>(const vec3 vertex[4], triangle &t);
-
-	friend std::ostream& operator<<(std::ostream &output, const triangle &t);
-
 };
 
 struct plane {
 
-	plane (vec3 n, float d=0.0f);
+	plane (vec3 n, float d=0.0f):normal(n), distance(d) {} 
 	
-	void create_plane (vec3 n, float d=0.0f);
-
 	float distanceFromPoint (vec3 point);
 
 	vec3 normal; float distance;
@@ -63,37 +42,125 @@ struct plane {
 
 struct linesegment {
 	
-	linesegment (vec3 point1, vec3 point2);
+	linesegment (vec3 point1, vec3 point2) : startpoint (point1), endpoint (point2) {}
 	
 	vec3 startpoint, endpoint;
 };
 
 struct slice {
 
-	std::vector<linesegment> slice;
+	vector<linesegment> Slice;
 	
-	void display_slice ();
-
-	void write_slice ();
-
+	void display_slice ( void );
 };
 
 class triangleMesh {
 	
-	std::vector <triangle> mesh;
+	vector <triangle> mesh;
 
 public:
 	void slice_mesh (plane *p, slice *s) ;
 
 	void displayMEsh (triangle &t);
 
-	void push_back (triangle t);
-
 	void display_all_elements ();
 
 	void find_min_max_var_z (float &min_z, float &max_z);
 	
 	int readStlFile (const char *filename);
-	//ideally functionality to read stl file should exist here
 };
+
+float plane::distanceFromPoint (vec3 point) {
+	normal = glm::normalize (normal);
+	return dot(point, normal) - distance;
+}	
+
+void slice::display_slice () {
+		for (auto sliceIterator = Slice.begin(); sliceIterator != Slice.end(); sliceIterator++ ) {
+			cout << "S " << to_string ( sliceIterator->startpoint ) << " ";
+			cout << "E " << to_string(sliceIterator->endpoint) << " || ";
+		}
+		cout<<"\n";
+	}
+
+
+void triangleMesh::display_all_elements () {
+
+	int counter=0;
+
+	cout<<"\nData in Mesh : ";
+	for( auto meshIterator=mesh.begin();meshIterator!=mesh.end();meshIterator++ )				
+		cout<<"\nTriangle No. "<<++counter<<" "<<*meshIterator;
+
+}
+
+
+
+int triangleMesh::readStlFile ( const char *filename ) {
+			
+	bool isASCII=false; //false -binary, true -ASCII
+
+		triangle t;
+		vec3 vertex[4];
+
+		//detect if the file is ASCII or not
+		int parser;
+		
+		FILE *file = fopen(filename, "r");
+			//Using C style file IO over C++ style streams, as streams cause data corruption.
+			//May be fixable, need to check advantages of both approaches
+			
+		do{
+			parser=fgetc(file);
+		}while((parser!=EOF) && parser<=127 );
+			//Check if file is ASCII. If true, every character read from file should be <=127
+			//Need to check for robustness of this approach.
+			
+		fclose(file);
+
+		if(parser==EOF)
+			isASCII=true;
+
+		if(!isASCII) {
+			//block to read binary stl file
+			
+				char modelName[80], discarder[2];
+				size_t facetNo;
+
+				cout<<"\nFile is not ASCII, using binary format";
+
+				file = fopen(filename, "rb");
+				
+				if(!file) {
+					cout<<"\nfile opening failed,  with error ";
+					perror("Error");
+					return 1;
+				}
+				
+				fread(modelName, 80, 1, file);
+				fread((void *)&facetNo, 4, 1, file);
+				
+				cout<<"\nNo. Of Facets is : "<<facetNo;
+				cin.get();
+
+
+				while(!feof(file)){
+					for(int i=0;i<4;i++){
+
+						fread((void *)&vertex[i].x, 4, 1 , file);
+						fread((void *)&vertex[i].y, 4, 1 , file);
+						fread((void *)&vertex[i].z, 4, 1 , file);
+				}
+						
+				vertex>>t;
+
+					mesh.push_back(t);
+
+					fread((void *)discarder, 2, 1, file);
+						//discard 2 bytes end of each loop
+				}
+			return 0;
+		}
+		return 1;
+}
 

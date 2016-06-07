95c8f4e0a4a062c3c4b891cec18876f2a503ea65
Brahm Prakash Mishra <brahm@live.in>
2016-06-07 11:03:37 +0530

    OpenGL Version changed to 4.0, for tesselation features

--- bolt_code/stlparse_opengl/graphics.cpp
+++ bolt_code/stlparse_opengl/graphics.cpp
@@ -72,8 +72,8 @@ GLFWwindow* glInit(int slicecounter, const GLfloat xshift, const GLfloat yshift,
 	if (!glfwInit())
 	    exit(EXIT_FAILURE);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);

3a0cfd26b79fe31810c763205bdcc7599e40c00e
Akshay <akshay.r1995@gmail.com>
2016-06-28 11:36:51 +0530

    Started documentation. Removed slice_mesh and store_slice methods.

--- bolt_code/meshDef.hpp
+++ bolt_code/meshDef.hpp
@@ -1,3 +1,12 @@
+/** 	
+* @file meshDef.hpp
+* \brief Header file for the mesh.
+*	
+* Defines the structures that represent components of the mesh, the
+* inersection planes and the slices.
+*/
+
+
 #ifndef MESHDEF_H
 #define MESHDEF_H
 
@@ -16,19 +25,38 @@ ostream &operator<<(ostream & output, const vec3 &vector ) {
 	return output;
 }
 
+/**
+ \brief Mesh triangle
+	
+	Represents one triangle in a mesh. 
+	Specified by 3 vertices and a normal vector.
+*/
 struct triangle {
 	
-	vec3 vertex[3], normal;
+	vec3 vertex[3]; ///< The three vertices of the triangle
+	vec3 normal;	///< Outward normal vector of the triangle
 
+	/**
+		Overloaded >> operator to read a triangle into a vec3 array of size 4.
+
+		@param vertex Index 0 is the normal. Index 1,2,3 represent the vertices.
+		@param &t Reference to the triangle variable.
+	*/
 	friend triangle& operator>>(const vec3 vertex[4], triangle &t){ 
 
 		t.normal = vertex[0];	
 		for(int i=0;i<3;i++) 
 			t.vertex[i] 	= vertex[i+1];
 		
-		//vertex[0] is normal, vertex 1, 2, 3 are the vertices. Read accordingly.
 		return t;
 	}
+
+	/**
+	 	Overloaded << operator to write a triangle into an output stream.
+
+	 	@param output Reference to the output stream.
+	 	@param t Reference to the triangle variable.
+	*/
 	friend ostream& operator<<(ostream &output, const triangle &t) {
 		
 		output	<<"\nVertex 1 : "<<t.vertex[0].x<<"i+"<<t.vertex[0].y<<"j+"<<t.vertex[0].z<<"k"
@@ -41,12 +69,30 @@ struct triangle {
 	
 };
 
+/**
+ \brief Intersection plane
+	
+	Specified by a normal vector and distance from some reference point.
+*/
+
 struct plane {
 	
+	/**
+		Returns the <i>signed</i> distance of the plane from a given point after normalizing the normal vector.
+	
+		@param point The point from which <i>signed</i> distance is to be calculated.
+	*/
 	float distanceFromPoint (vec3 point);
 
-	vec3 normal; float distance;
+	vec3 normal;	///< Normal vector of the plane.
+	float distance; ///< Signed distance of the plane from a reference point.
 
+	/**
+		Instantiates members <c>normal</c> and <c>distance</c> with the given arguments.
+	
+		@param n Normal vector.
+		@param d Distance from a reference point.
+	*/
 	void create_plane (vec3 n, float d=0.0f ){
 			normal=n;
 			distance=d;
@@ -54,20 +100,40 @@ struct plane {
 
 };
 
+/**
+ \brief Line segment in a slice
+
+	Specified by start and end points.
+*/
 struct linesegment {
 	
+	/**
+		Constructor to initialize <c>startpoint</c> and <c>endpoint</c>.
+	
+		@param point1 Start point.
+		@param point2 End point.
+	*/
 	linesegment (vec3 point1, vec3 point2) : startpoint (point1), endpoint (point2) {}
 	
-	vec3 startpoint, endpoint;
+	vec3 startpoint; ///< Vector representing the start point of the line segment.
+	vec3 endpoint;   ///< Vector representing the end point of the line segment. 
 };
+
+/**
+ \brief Slice
+
+	Represents a 2D cross section of the model.
+	Specified by a set of linesegments.
+*/
 
 struct slice {
 
-	vector<linesegment> slice;
+	vector<linesegment> slice;	///< List of line segments that represent the slice. 
 	
+	/**
+	*/
 	void display_slice ( void );
-
-	void store_slice (string &filename,const int sliceNo);
+	
 };
 
 class stlMesh {
